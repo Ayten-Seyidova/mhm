94bdb7fcc634ed130b9341707b964c424055a0c1
mirikhann <mr.sahib97@gmail.com>
2025-01-21 01:22:33 +0400

    add model

--- app/Models/NotificationParameters.php
+++ app/Models/NotificationParameters.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class NotificationsParameters extends Model
+class NotificationParameters extends Model
 {
     use HasFactory;
-    protected $table = 'notification_parameters';
 }
