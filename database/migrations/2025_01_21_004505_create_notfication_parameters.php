29bc3b40b55d481bcf6647a691210cefebe819b4
mirikhann <mr.sahib97@gmail.com>
2025-01-21 00:56:04 +0400

    notify params updated

---
+++ database/migrations/2025_01_21_004505_create_notfication_parameters.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notfication_parameters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('notificationType');
+            $table->string('token',255)->default(null);
+            $table->string('deviceId',255);
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notfication_parameters');
+    }
+};
