639abd90849f2b9ef9b2131ce0d58030fbfa20ff
Albert <alberthaciverdiyev55@gmail.com>
2025-09-09 14:07:22 +0400

    added logging for phone number and user retrieval in AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,6 +12,7 @@ use App\Models\Group;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
@@ -153,9 +154,9 @@ class AuthController extends Controller
             'password'=>'required',
         ]);
         $phoneNumber = str_replace(['+','_',''], '',$parameters['phoneNumber']);
-
+        Log::info($phoneNumber);
         $user = Guest::where("phone",$phoneNumber)->first();
-
+        Log::info($user);
         if ($user && Hash::check($parameters['password'], $user->password)) {
 
 //            $param = [
