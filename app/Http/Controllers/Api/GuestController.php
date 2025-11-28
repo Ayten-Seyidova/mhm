95bdd9d5f34a17c22fe51ab544126830e1dbcbfc
Albert <alberthaciverdiyev55@gmail.com>
2025-11-28 16:52:29 +0400

    commented out sub_direction filter in GuestController to disable unused query constraint

--- app/Http/Controllers/Api/GuestController.php
+++ app/Http/Controllers/Api/GuestController.php
@@ -103,9 +103,9 @@ class GuestController extends Controller
             ->where('status',1)
             ->where("is_deleted",0)
             ->where('user_id', $teacherId)
-            ->whereHas('guestExamSubDirections',function($q) use($request){
-                $q->where('sub_direction_id', $request->user()->sub_direction_id);
-            })
+//            ->whereHas('guestExamSubDirections',function($q) use($request){
+//                $q->where('sub_direction_id', $request->user()->sub_direction_id);
+//            })
         ;
 //dd($request->user()->sub_direction_id);
        // dd($request->user());
