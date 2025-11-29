3faa6e0df426e0fc9cffb2edee63a58676ab213d
Albert <alberthaciverdiyev55@gmail.com>
2025-11-29 16:51:07 +0400

    updated condition in NotificationsGuestController to use integer value for 'all' column filter

--- app/Http/Controllers/Api/NotificationsGuestController.php
+++ app/Http/Controllers/Api/NotificationsGuestController.php
@@ -16,7 +16,7 @@ class NotificationsGuestController extends Controller
         $orderBy = $_GET['orderBy'] ?? null;
         $user_id = $request->user()->id;
         // $list = GuestNotification::where("guest_id", $user_id);
-        $list = GuestNotification::where("all", true);
+        $list = GuestNotification::where("all", 1);
         $count = count($list->get());
         dd($list->get());
 
