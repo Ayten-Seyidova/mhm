032dff9d8b2b69bd7e15a203c1a1ce74b51ac6aa
nalse <54538144+nalse@users.noreply.github.com>
2026-04-13 13:52:17 +0400

    Create SendGuestNotificationChunk.php

---
+++ app/Jobs/SendGuestNotificationChunk.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\Helpers\FirebaseHelper;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendGuestNotificationChunk implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $timeout = 120;
+    public $tries = 3;
+    public $backoff = [30, 120, 300];
+
+    public $title;
+    public $desc;
+    public $tokens;
+
+    public function __construct($title, $desc, $tokens = [])
+    {
+        $this->title = $title;
+        $this->desc = $desc;
+        $this->tokens = $tokens;
+    }
+
+    public function handle()
+    {
+        if (empty($this->tokens)) {
+            return;
+        }
+
+        FirebaseHelper::sendTokens($this->title, $this->desc, $this->tokens);
+    }
+}
